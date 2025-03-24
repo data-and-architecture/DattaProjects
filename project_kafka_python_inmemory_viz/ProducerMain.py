d8bd3595b4ac18e1bffca5323574c7b6bf40fc72
data-and-architecture <154007149+data-and-architecture@users.noreply.github.com>
2025-03-24 15:51:24 +0300

    Add files via upload
    commit

---
+++ project_kafka_python_inmemory_viz/ProducerMain.py
@@ -0,0 +1,38 @@
+
+import yaml
+import configuration
+from PythonProducer import producerControl
+from PythonConsumer import consumerControl
+
+
+# From the Config file , loading file path and name.
+def loadConsuerParamters():
+    with open('ConsumerConfig.yaml', 'r') as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
+    # Print the values as a dictionary
+    return (data)
+
+
+# From the Config file , loading file path and name.
+def loadProducerParamters():
+    with open('ProducerConfig.yaml', 'r') as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
+    # Print the values as a dictionary
+    return (data)
+
+
+
+def main():
+    print('Scheduling pgm. Can be replaced with AirFlow')
+    pparam = loadProducerParamters()
+    producerControl.control().Controller(pparam)
+    #cparam = loadConsuerParamters()
+    #consumerControl.control().Controller(cparam)
+
+
+    
+    
+
+
+if __name__ =='__main__':
+    main()
---
+++ project_kafka_python_inmemory_viz/configuration.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.append('project_kafka_python_inmemory_viz/PythonProducer')
+sys.path.append('project_kafka_python_inmemory_viz/PythonConsumer')
